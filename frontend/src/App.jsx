5dcf42c3c35c818ad82ac48caa2daf58c9e2cb43
Matthieu <Matthieu.Maginot@gmail.com>
2023-02-02 17:06:39 +0100

    admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import PokeDex from "./pages/PokeDex";
 import Catch from "./pages/Catch";
 import Bag from "./pages/Bag";
+import Profile from "./pages/Profile";
 import { useCurrentUserContext } from "./contexts/CurrentUserContext";
 
 import "./App.css";
@@ -20,6 +21,7 @@ function App() {
           {currentUser.id && <Route path="/pokedex" element={<PokeDex />} />}
           {currentUser.id && <Route path="/catch" element={<Catch />} />}
           {currentUser.id && <Route path="/bag" element={<Bag />} />}
+          {currentUser.id && <Route path="/profile" element={<Profile />} />}
           <Route path="*" element={<Login />} />
         </Routes>
       </main>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,15 @@ export default function Home() {
 
   const navigate = useNavigate();
 
+  const handleLogOut = () => {
+    navigate("/");
+  };
+
+  const handleProfile = (e) => {
+    e.preventDefault();
+    navigate("/profile");
+  };
+
   const handleNavigate = (e) => {
     e.preventDefault();
     navigate("/pokedex");
@@ -43,7 +52,7 @@ export default function Home() {
         <div className="second-container-first">
           <span
             style={{
-              color: `${currentUser.roleID === 3 ? "green" : "white"}`,
+              color: `${currentUser.roleID === 3 ? "#3AFF00" : "white"}`,
             }}
           >
             {currentUser.username}
@@ -55,15 +64,35 @@ export default function Home() {
               src="https://archives.bulbagarden.net/media/upload/9/9a/Spr_B2W2_Red.png"
               alt="sprite-trainer-animated"
             />
-            <img
-              src="https://www.pokencyclopedia.info/sprites/3ds/ani_6_shiny/3ani_-S_384-mega__oras.gif"
-              alt="sprite-pokemon-animated"
-            />
+            {currentUser.roleID === 3 ? (
+              <img
+                src="https://play.pokemonshowdown.com/sprites/xyani-shiny/zygarde-complete.gif"
+                alt="sprite-pokemon-animated"
+              />
+            ) : (
+              <img
+                src="https://www.pokencyclopedia.info/sprites/3ds/ani_6_shiny/3ani_-S_384-mega__oras.gif"
+                alt="sprite-pokemon-animated"
+              />
+            )}
           </div>
         </div>
-        <div className="second-container-third">hello</div>
+        <div className="second-container-third">
+          <button type="button" onClick={handleProfile}>
+            Profile
+          </button>
+          <button type="button" onClick={handleLogOut}>
+            Log Out
+          </button>
+        </div>
       </div>
-      <div className="third-container" />
+      <div className="third-container">
+        <div className="chat-container">
+          <div className="chat-container-title">
+            <span>Chat</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
